e95d9319446dcfb6b3ad27f0335b4fdf42201ee2
Param Mangukiya <mangukiyaparam11@gmail.com>
2026-06-23 16:52:15 +0530

    Add basic SQL queries for employee data retrieval

---
+++ sql/basic_queries.sql
@@ -0,0 +1,14 @@
+SELECT * FROM employees;
+
+SELECT name FROM employees;
+
+SELECT name, salary
+FROM employees;
+
+SELECT *
+FROM employees
+WHERE salary > 45000;
+
+SELECT *
+FROM employees
+ORDER BY salary DESC;
